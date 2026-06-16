7970b12b6d9edc73b19676798f2076dc34405184
joxyzan <johan.hagvil@gmail.com>
2026-06-16 10:05:59 +0200

    user ID change

--- functions/loopis-cronjobs-reminders-functions.php
+++ functions/loopis-cronjobs-reminders-functions.php
@@ -28,34 +28,34 @@ function reminder_leave(int $reminder_leave, int $post_id) {
 		💡 Påminnelse #1 att lämna i skåpet @' . $author_name . ' <br>
 		✅ Tryck &quot;Lämnat&quot; på LOOPIS.app när du har lämnat. <br>
 		🔓 Kod till skåpet: <b>' . $locker_code . '</b>
-        ', $post_id, 1, $author_id);
+        ', $post_id, 2, $author_id);
 		
         add_admin_comment('<p class="reminder">
 		💡 Påminnelse #1 att lämna i skåpet <span>🔔' . $author_name . '</span>
-		</p>', $post_id, 1);
+		</p>', $post_id, 2);
 
     } elseif ($number == 2) {
         send_admin_notification_email('
 		💡 Påminnelse #2 att lämna i skåpet @' . $author_name . ' <br>
 		😍 ' . $fetcher_name . ' väntar på att få hämta... <br>
 		🔓 Kod till skåpet: <b>' . $locker_code . '</b>
-        ', $post_id, 1, $author_id);
+        ', $post_id, 2, $author_id);
 		
         add_admin_comment('<p class="reminder">
 		💡 Påminnelse #2 att lämna i skåpet <span>🔔' . $author_name . '</span>
-		</p>', $post_id, 1);
+		</p>', $post_id, 2);
 
     } elseif ($number == 3) {
         send_admin_notification_email('
 		⚠ Påminnelse #3 att lämna i skåpet @' . $author_name . ' <br>
 		🗨 Skriv gärna i en kommentar till ' . $fetcher_name . ' om/när du kommer att lämna. <br>
 		🔓 Kod till skåpet: <b>' . $locker_code . '</b>
-        ', $post_id, 1, $author_id);
+        ', $post_id, 2, $author_id);
 		
         send_admin_notification_email('
 		💡 Vi har nu skickat tre påminnelser till ' . $author_name . ' att lämna i skåpet. <br>
 		💚 Beklagar fördröjningen! @' . $fetcher_name . '
-        ', $post_id, 1, $fetcher_id);
+        ', $post_id, 2, $fetcher_id);
 		
         add_admin_comment(
             '<p class="reminder">' .
@@ -92,29 +92,29 @@ function reminder_fetch(int $reminder_fetch, int $post_id) {
         💡 Påminnelse #1 att hämta i skåpet @' . $fetcher_name . ' <br>
         ☑ Tryck &quot;Hämtat&quot; på LOOPIS.app när du har hämtat. <br>
         🔓 Kod till skåpet: <b>' . $locker_code . '</b>
-        ', $post_id, 1, $fetcher_id);
+        ', $post_id, 2, $fetcher_id);
 
         add_admin_comment('<p class="reminder">
         💡 Påminnelse #1 att hämta i skåpet <span>🔔' . $fetcher_name . '</span>
-		</p>', $post_id, 1);
+		</p>', $post_id, 2);
 
     } elseif ($number == 2) {
         send_admin_notification_email('
         💡 Påminnelse #2 att hämta i skåpet @' . $fetcher_name . ' <br>
         ♻ För att skåpet inte ska bli fullt önskar vi att du hämtar. <br>
         🔓 Kod till skåpet: <b>' . $locker_code . '</b>
-        ', $post_id, 1, $fetcher_id);
+        ', $post_id, 2, $fetcher_id);
 
         add_admin_comment('<p class="reminder">
         💡 Påminnelse #2 att hämta i skåpet <span>🔔' . $fetcher_name . '</span>
-		</p>', $post_id, 1);
+		</p>', $post_id, 2);
 
     } elseif ($number == 3) {
         send_admin_notification_email('
         ⚠ Påminnelse #3 att hämta i skåpet @' . $fetcher_name . ' <br>
         🗨 Skriv gärna i en kommentar till LOOPIS när du kommer att hämta. <br>
         🔓 Kod till skåpet: <b>' . $locker_code . '</b>
-        ', $post_id, 1, $fetcher_id);
+        ', $post_id, 2, $fetcher_id);
 
         add_admin_comment(
         '<p class="reminder">' .
@@ -154,22 +154,22 @@ function reminder_custom(int $reminder_fetch, int $post_id) {
         💡 Påminnelse att ta kontakt @' . $fetcher_name . ' <br>
         📱 Har du skickat ett sms till ' . $author_name . ' på <a href="sms:' . $author_phone .'">' . $author_phone .'</a>? <br>
         📍 Adress för hämtning: ' . $location . ' <br>
-        ', $post_id, 1, $fetcher_id);
+        ', $post_id, 2, $fetcher_id);
         
         add_admin_comment('<p class="reminder">
         💡 Påminnelse att ta kontakt <span>🔔' . $fetcher_name . '</span>
-        </p>', $post_id, 1);
+        </p>', $post_id, 2);
 
     } elseif ($number == 2) {
         send_admin_notification_email('
         💡 Påminnelse att hämta @' . $fetcher_name . ' <br>
         📱 Hoppas att du har kontakt med ' . $author_name . ' nu! <br>
         ☑ Tryck &quot;Hämtat&quot; på LOOPIS.app när du har hämtat. <br>
-        ', $post_id, 1, $fetcher_id);
+        ', $post_id, 2, $fetcher_id);
         
         add_admin_comment('<p class="reminder">
         💡 Påminnelse att hämta <span>🔔' . $fetcher_name . '</span>
-        </p>', $post_id, 1);
+        </p>', $post_id, 2);
 
     } elseif ($number == 3) {
         $fetcher_phone = get_user_meta($fetcher_id, 'wpum_phone', true);
@@ -177,7 +177,7 @@ function reminder_custom(int $reminder_fetch, int $post_id) {
         💡 Nu har det gått tre dagar sedan ' . $fetcher_name . ' paxade. <br>
         📱 Om ni inte har kontakt ännu, skicka ett sms till <a href="sms:' . $fetcher_phone .'">' . $fetcher_phone .'</a> <br>
         💚 Hoppas hämtningen går bra! @' . $author_name . '.
-        ', $post_id, 1, $author_id);
+        ', $post_id, 2, $author_id);
 
     } else {
         return 0;
