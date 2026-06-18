1896cb3626088bc528840b8a95609a1c97f970da
huberthilborn <Hubert.hilborn@hotmail.com>
2026-06-18 12:10:22 +0200

    safe!

--- functions/loopis-cronjobs-raffle.php
+++ functions/loopis-cronjobs-raffle.php
@@ -161,7 +161,7 @@ function loopis_cronjobs_raffle_network() {
 		$exclude = get_option('loopis_excluded_raffle') ?? false;
 		foreach ($sites as $site){
 			if ($exclude){
-				if (in_array($site, $exclude)){
+				if (in_array($site, (array) $exclude)){
 					continue;
 				}
 			}
